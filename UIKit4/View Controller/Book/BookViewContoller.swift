dad151a3cd4e390de21de2311776c1179e901e26
songmoro <wlqgkr@naver.com>
2025-07-28 12:50:10 +0900

    페이지네이션

--- UIKit4/View Controller/Book/BookViewContoller.swift
+++ UIKit4/View Controller/Book/BookViewContoller.swift
@@ -11,6 +11,9 @@ import SnapKit
 
 class BookViewContoller: UIViewController {
     var item = BookResponse(items: []).items
+    var item2: [KakaoBookResponse.Document] = []
+    var meta = KakaoBookResponse.Meta(is_end: false, pageable_count: 0, total_count: 0)
+    var page = 1
     
     lazy var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
@@ -59,27 +62,39 @@ extension BookViewContoller: Designable {
 extension BookViewContoller: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let text = searchBar.text else { return }
-        request(text)
+        requestKakao(text)
     }
 }
 
 extension BookViewContoller: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        item.count
+        item2.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(BookCell.self, for: indexPath)
         
         if let cell = cell as? BookCell {
-            let row = item[indexPath.row]
-            cell.titleLabel.text = row.title
+//            let row = item2[indexPath.row]
+//            cell.titleLabel.text = row.title
 //            cell.thumbnailImageView.image = UIImage
-            cell.subTitleLabel.text = "\(row.author) 작가(\(row.pubdate))"
-            cell.overviewLabel.text = row.description
+//            cell.subTitleLabel.text = "\(row.author) 작가(\(row.pubdate))"
+//            cell.overviewLabel.text = row.description
+            
+            let row = item2[indexPath.row]
+            cell.titleLabel.text = row.title
+            cell.subTitleLabel.text = "\(row.authors.description) 작가(\(row.datetime))"
+            cell.overviewLabel.text = row.contents
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if !meta.is_end, indexPath.row == (item2.count - 3) {
+            page += 1
+            requestKakao(searchBar.text!)
+        }
     }
     
     func request(_ text: String) {
@@ -100,6 +115,52 @@ extension BookViewContoller: UITableViewDelegate, UITableViewDataSource {
                     break
                 }
             }
+    }
+    
+    func requestKakao(_ text: String) {
+        let url = URL(string: "https://dapi.kakao.com/v3/search/book?query=\(text)&size=20&page=\(page)")!
+        let header = HTTPHeaders([
+            "Authorization": APIKey.kakaoAuthorization
+        ])
+        
+        AF.request(url, method: .get, headers: header)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: KakaoBookResponse.self) {
+                switch $0.result {
+                case .success(let book):
+                    self.item2.append(contentsOf: book.documents)
+                    self.meta = book.meta
+                    self.tableView.reloadData()
+                default:
+                    break
+                }
+            }
+    }
+}
+
+struct KakaoBookResponse: Decodable {
+    let documents: [Document]
+    let meta: Meta
+    
+    struct Document: Decodable {
+        let authors: [String]
+        let contents: String
+        let datetime: String
+        let isbn: String
+        let price: Int
+        let publisher: String
+        let sale_price: Int
+        let status: String
+        let thumbnail: String
+        let title: String
+        let translators: [String]
+        let url: String
+    }
+
+    struct Meta: Decodable {
+        let is_end: Bool
+        let pageable_count: Int
+        let total_count: Int
     }
 }
 
